69e5393bee1d644bde73712f098efd28472d667f
Gregory Koberger <gkoberger@gmail.com>
2017-07-03 18:46:39 -0700

    v1.0.2, now with download URL

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
 
 setuptools.setup(name='api',
-                 version='1.0.0',
+                 version='1.0.2',
                  description='Consume an API from ReadMe Build',
                  long_description=open('README.md').read().strip(),
                  author='ReadMe',
                  author_email='support@readme.io',
+                 download_url='https://github.com/readmeio/api-python/archive/1.0.2.tar.gz',
                  url='http://readme.build',
                  py_modules=['api'],
                  install_requires=[],
