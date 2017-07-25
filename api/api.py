04344817a77d4701b9f247078579a235380e51d3
Gregory Koberger <gkoberger@gmail.com>
2017-07-24 17:56:20 -0700

    Support organizations and versioning

--- api/api.py
+++ api/api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import json
+import re
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -18,20 +19,34 @@ class api():
     def __init__(self, key):
         self.key = key
 
-    def __call__(self, project):
-        request = Request(project=project, key=self.key)
+    def __call__(self, service):
+        request = Request(service=service, key=self.key)
         return request
 
 class Request():
-    def __init__(self, project, key):
-        self.project = project
+    def __init__(self, service, key):
+        self.service = service
         self.key = key
 
     def run(self, method, data):
-        out = requests.post('https://api.readme.build/v0/services/%s/%s/invoke' % (self.project, method),
+        split = re.search('(?:([-\w]+)/)?([-\w]+)(?:@([-.\w]+))?', self.service)
+        organization = split.group(1)
+        service = split.group(2)
+        version_override = split.group(3)
+
+        # Prefixes the organization (including an @) if there's an org
+        # So, 'twitter/math' becomes '@twitter/math'
+        service_full = '@%s/%s' % (organization, service) if organization else service
+
+        headers = {}
+        if version_override:
+            headers = {'X-Build-Version-Override': version_override}
+
+        out = requests.post('https://api.readme.build/v0/services/%s/%s/invoke' % (service_full, method),
             data=data,
+            headers=headers,
             auth=HTTPBasicAuth(self.key, '')
-            )
+        )
 
         result = Response()
 
@@ -43,7 +58,7 @@ class Request():
         result.is_deprecated = (out.headers.setdefault('X-Build-Deprecated', 'false') == 'true')
 
         if result.is_deprecated:
-            service = self.project
+            service = self.service
             version = out.headers['X-Build-Version']
             console.warn('%s v%s is deprecated! Run `api update %s` to use the latest version' % (
                 service, version, service
